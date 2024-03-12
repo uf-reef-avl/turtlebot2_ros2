7c7c397854634c34eebf6067b4467de2a64efdbf
Jared Paquet <jaredpaquet@ufl.edu>
2024-03-12 18:25:23 -0500

    Update readme add install script and backup of webguide

---
+++ install.sh
@@ -0,0 +1,12 @@
+cwd=$(pwd)
+cd ~/
+wget https://raw.githubusercontent.com/kobuki-base/kobuki_ftdi/devel/60-kobuki.rules
+sudo cp 60-kobuki.rules /etc/udev/rules.d
+sudo service udev reload
+sudo service udev restart
+sudo apt update
+sudo apt install ros-humble-kobuki-velocity-smoother ros-humble-teleop-twist-keyboard ros-humble-joy-teleop ros-humble-teleop-twist-joy
+cd cwd
+cd ../..
+rosdep install -i --from-path src --rosdistro humble -y
+colcon build --symlink-install --executor sequential --cmake-args -Wno-dev
